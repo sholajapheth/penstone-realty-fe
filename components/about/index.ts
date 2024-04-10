af46d956a2e5f3c78b308c2614c5001e373e924d
sholajapheth <sholajapheth@gmail.com>
2024-04-10 16:11:46 +0100

    dep fixes

--- components/about/index.ts
+++ components/about/index.ts
@@ -1,2 +1,3 @@
 export { default as AboutPageBanner } from "./AboutPageBanner";
 export { default as AboutServices } from "./AboutServices";
+export { default as AboutMission } from "./AboutMission";
