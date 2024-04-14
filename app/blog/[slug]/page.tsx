b98f15afb19895372eeb8261f70e42b0dd3933a7
Desmond Fon <desmondfon@yahoo.com>
2024-04-14 17:34:30 +0100

    wip

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -20,25 +20,25 @@ const Page = () => {
   return (
     <div>
       <div className="bg-[#F5FCFF80] px-[1em] lg:px-[14em] py-[2em] lg:py-[6em]">
-        <p className="text-secondary lg:leading-[120px] text-[30px] lg:text-[70px] font-bold">
+        <p className="text-secondary lg:leading-[120%] text-[30px] lg:text-[70px] font-semibold w-full lg:w-[70%]">
           How to attract buyers and close deals
         </p>
       </div>
 
-      <div className="px-[1em] py-[1em] lg:px-[14em]">
-        <div className="flex items-center justify-between ">
+      <div className="px-[1em] pt-[2em] pb-[1.5em] lg:px-[18em]">
+        <div className="flex items-end justify-between ">
           <div className="flex items-center gap-4">
             <Image
               src={"/img/sample-avatar.png"}
-              height={50}
-              width={50}
+              height={55}
+              width={55}
               alt="blog-author"
               className="rounded-full"
             />
 
             <div>
               <p className="font-bold text-secondary">Tope Akinmegha</p>
-              <p className=" text-[#7E8492]">Registered ESV</p>
+              <p className=" text-[#7E8492] pt-2">Registered ESV</p>
             </div>
           </div>
           <p className=" text-[#7E8492]">10 min read May 20, 2023</p>
@@ -52,7 +52,22 @@ const Page = () => {
             className="w-full h-full"
           />
 
-          <div className="mt-10 md:flex justify-start items-start gap-[20px]  ">
+          <div className="pt-[40px] lg:pt-[80px]">
+            <p className="text-[18px] lg:text-[24px] text-[#1D1E25]">
+              In the dynamic world of real estate, success hinges on the ability
+              to understand and cater to the needs of buyers. Whether
+              you&apos;re a seasoned agent or a homeowner looking to sell,
+              adopting a customer-centric approach is key to attracting buyers
+              and closing deals effectively. In this guide, we&apos;ll{" "}
+              <span className="font-bold">
+                explore three essential steps to help you navigate the real
+                estate market with confidence and achieve your selling
+                objectives.
+              </span>
+            </p>
+          </div>
+
+          <div className="mt-10 md:flex justify-start items-start gap-[48px] ">
             <Image
               src={"/img/sample-avatar.png"}
               width={300}
@@ -84,14 +99,14 @@ const Page = () => {
               <div className="flex flex-col gap-4 mt-4">
                 <p>Written by</p>
                 <p
-                  className="text-2xl font-bold text-secondary cursor-pointer"
+                  className="text-[32px] font-bold text-secondary cursor-pointer"
                   onClick={() => router.push("/listing")}
                 >
                   Tope Akinmegha
                 </p>
-                <p className="font-semibold text-gray-400">Registered ESV</p>
+                <p className="font-medium text-[#7E8492]">Registered ESV</p>
 
-                <p className="w-full lg:w-[80%] leading-[2em]">
+                <p className="w-full text-[18px] leading-[2em]">
                   Skilled in React Native, iOS and backend, Toni has a
                   demonstrated knowledge of the information technology and
                   services industry, with plenty of hands-on experience to back
@@ -102,38 +117,40 @@ const Page = () => {
           </div>
         </div>
 
-        <div className="flex items-center justify-between mt-8">
+        <div className="flex items-center justify-between mt-[3em] lg:mt-[5em]">
           <div></div>
 
-          <div>
-            <p className="font-semibold text-gray-400 flex items-center gap-2">
+          <div className="w-full lg:w-[30%]">
+            <p className="font-semibold text-[#9EA3AE] flex items-center gap-2 text-[18px]">
               Next Post <CgChevronRight className="font-bold" size={25} />
             </p>
-            <p className="font-bold mt-2">
+            <p className="font-semibold text-[#282828] text-[20px] mt-2">
               The Benefits of Working with a Real Estate Agent
             </p>
           </div>
         </div>
       </div>
-      <div className="mb-4 md:mb-8 mt-[5em] p-[1em] lg:p-[4em]">
-        <div className="flex items-center justify-between my-8">
-          <p className="font-semibold text-[25px] md:text-[40px] text-secondary">
-            Related Posts
-          </p>
-          <div className="lg:flex gap-4 hidden">
-            <button className="rounded-md bg-primary p-2  ">
-              <BiChevronLeft color="white" size={30} />
-            </button>
-            <button className="rounded-md bg-gray-200 p-2 text-primary  ">
-              <BiChevronRight size={30} />
-            </button>
-          </div>
-        </div>
 
-        <div className="flex items-center justify-center md:justify-between flex-wrap w-full gap-[32px]">
-          {[1, 2, 3].map((item) => (
-            <BlogCard key={item} />
-          ))}
+      <div className="bg-white flex  justify-center my-[2em] lg:my-[3em]">
+        <div className="w-[85%]">
+          <div className="flex items-center justify-between my-8">
+            <p className="font-semibold text-[25px] md:text-[40px] text-secondary">
+              Related Posts
+            </p>
+            <div className="lg:flex gap-4 hidden">
+              <button className="rounded-md bg-primary p-2  ">
+                <BiChevronLeft color="white" size={30} />
+              </button>
+              <button className="rounded-md bg-gray-200 p-2 text-primary  ">
+                <BiChevronRight size={30} />
+              </button>
+            </div>
+          </div>
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+            {[1, 2, 3].map((item) => (
+              <BlogCard key={item} />
+            ))}
+          </div>
         </div>
       </div>
 
--- components/blog/BlogBody.tsx
+++ components/blog/BlogBody.tsx
@@ -3,26 +3,27 @@ import BlogCard from "./BlogCard";
 
 const BlogBody = () => {
   return (
-    <div className="px-[1em] md:px-[4em] lg:px-[6em] mb-[3em]">
-      <div className="flex items-center justify-center my-4">
-        <div className="flex gap-4 items-center flex-wrap text-xs font-semibold">
-          <button className="px-3 py-2 rounded-xl text-primary bg-[#FFD7E0]">
-            All Post
-          </button>
-          <button>Webminar</button>
-          <button>Leisure</button>
-          <button>Blog</button>
-          <button>E-Book</button>
+    <div className="bg-white flex  justify-center">
+      <div className="w-[85%] mb-[4em]">
+        <div className="flex items-center justify-center my-4">
+          <div className="flex gap-4 items-center flex-wrap text-xs font-semibold">
+            <button className="px-3 py-2 rounded-xl text-primary bg-[#FFD7E0]">
+              All Post
+            </button>
+            <button>Webminar</button>
+            <button>Leisure</button>
+            <button>Blog</button>
+            <button>E-Book</button>
+          </div>
         </div>
-      </div>
-
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-        <BlogCard />
-        <BlogCard />
-        <BlogCard />
-        <BlogCard />
-        <BlogCard />
-        <BlogCard />
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+          <BlogCard />
+          <BlogCard />
+          <BlogCard />
+          <BlogCard />
+          <BlogCard />
+          <BlogCard />
+        </div>
       </div>
     </div>
   );
