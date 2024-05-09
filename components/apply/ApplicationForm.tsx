8312cca7396f462500ec80fa2bd1409d9ec63fae
Desmond-Fon <desmondfon@yahoo.com>
2024-05-09 17:12:59 +0100

    added form validation

--- components/apply/ApplicationForm.tsx
+++ components/apply/ApplicationForm.tsx
@@ -21,7 +21,7 @@ const ApplicationForm = () => {
     phoneNumber: "",
     email: "",
     address: "",
-    countryCode: "",
+    countryCode: "+234",
     dateOfBirth: "",
     monthlyRentBudget: "",
     employerName: "",
