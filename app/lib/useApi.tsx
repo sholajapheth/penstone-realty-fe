aeb36bbd1b23ed621a037a537834f3aa3b6ca9e1
Desmond-Fon <desmondfon@yahoo.com>
2024-05-22 23:13:52 +0100

    fixed loader issue

--- app/lib/useApi.tsx
+++ app/lib/useApi.tsx
@@ -5,68 +5,72 @@ import {
   useQuery,
   useQueryClient,
 } from "@tanstack/react-query";
-import { useAppToast } from "./useAppToast";
 import { AxiosError } from "axios";
+import { useAppToast } from "./useAppToast";
 
 interface ApiMutationOptions<TData, TVariables> {
   mutationFunction?: (variables: TVariables) => Promise<TData>;
-  onSuccess?: (data: TData) => void;
-  onError?: (error: AxiosError) => void;
   onSuccessFn?: (data: TData) => void;
+  onErrorFn?: (error: AxiosError) => void; // Added
 }
 
 export function useAPI() {
   const toast = useAppToast();
   const queryClient = useQueryClient();
 
-function useAPIMutation<TData, TVariables extends ((error: AxiosError<unknown, any>, variables: TVariables, context: unknown) => unknown) | undefined>({
-  mutationFunction,
-  onSuccessFn,
-}: ApiMutationOptions<TData, TVariables>): UseMutationResult<
-  TData,
-  AxiosError,
-  TVariables
-> {
-  return useMutation<TData, AxiosError, TVariables>({
-    mutationFn: mutationFunction,
-    onSuccess: onSuccessFn,
-    onError: (error: AxiosError) => {
-      if (error.response) {
-        console.log(error.response);
-        const status = error.response.status;
-        if (status === 400) {
-          toast({
-            description: "Please check your credentials",
-            status: "error",
-          });
-        } else if (status === 401) {
-          toast({
-            // @ts-ignore
-            description: error.response.data.message as string,
-            status: "error",
-          });
+  function useAPIMutation<TData, TVariables>({
+    mutationFunction,
+    onSuccessFn,
+    onErrorFn, // Added
+  }: ApiMutationOptions<TData, TVariables>): UseMutationResult<
+    TData,
+    AxiosError,
+    TVariables
+  > {
+    return useMutation<TData, AxiosError, TVariables>({
+      mutationFn: mutationFunction,
+      onSuccess: onSuccessFn,
+      onError: (error: AxiosError) => {
+        if (error.response) {
+          console.log(error.response);
+          const status = error.response.status;
+          if (status === 400) {
+            toast({
+              description: "Please check your credentials",
+              status: "error",
+            });
+          } else if (status === 401) {
+            toast({
+              description:
+                // @ts-ignore
+                (error.response.data.message as string) ||
+                "Invalid credentials",
+              status: "error",
+            });
+          } else {
+            toast({
+              description:
+                // @ts-ignore
+                "An error occurred: " + error?.response?.data.message,
+              status: "error",
+            });
+          }
         } else {
-          toast({
-            // @ts-ignore
-            description: "An error occurred" + error.response.data.message,
-            status: "error",
-          });
+          toast({ description: "An error occurred", status: "error" });
         }
-      } else {
-        toast({ description: "An error occurred", status: "error" });
-      }
-    },
-  });
-}
+
+        if (onErrorFn) {
+          onErrorFn(error); // Call the custom error handler
+        }
+      },
+    });
+  }
 
   function useAPIQuery<TData, TError>({
     queryFn,
     queryKey,
   }: any): UseQueryResult<TData, TError> {
-    return useQuery<TData, TError>({
-      queryKey,
-      queryFn,
-    });
+    return useQuery<TData, TError>({ queryKey, queryFn });
   }
 
   return { useAPIQuery, useAPIMutation, useQuery, useMutation, queryClient };
